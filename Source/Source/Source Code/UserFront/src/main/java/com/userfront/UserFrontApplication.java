4fc50d44b5649b6b1891be6dbc63a6dbf8889218
Tabet <b_tabet@esi.dz>
2021-12-14 19:46:36 +0100

    Projet UserFront

--- Source/Source/Source Code/UserFront/src/main/java/com/userfront/UserFrontApplication.java
+++ Source/Source/Source Code/UserFront/src/main/java/com/userfront/UserFrontApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserFrontApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UserFrontApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(UserFrontApplication.class, args);
+	}
 }
--- Source/Source/Source Code/UserFront/src/test/java/com/userfront/UserFrontApplicationTests.java
+++ Source/Source/Source Code/UserFront/src/test/java/com/userfront/UserFrontApplicationTests.java
@@ -1,13 +1,16 @@
 package com.userfront;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class UserFrontApplicationTests {
+public class UserFrontApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
