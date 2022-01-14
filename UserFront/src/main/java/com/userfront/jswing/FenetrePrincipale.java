ff42963471f26ed31b18c7c70e3189409988dc11
Tabet <b_tabet@esi.dz>
2022-01-14 22:02:15 +0100

    optimi code

--- UserFront/src/main/java/com/userfront/jswing/FenetrePrincipale.java
+++ UserFront/src/main/java/com/userfront/jswing/FenetrePrincipale.java
@@ -1,6 +1,6 @@
 package com.userfront.jswing;
 
-import net.proteanit.sql.DbUtils;
+import com.userfront.jswing.common.CommonSwing;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -8,34 +8,35 @@ import java.awt.event.ActionListener;
 import java.sql.*;
 
 public class FenetrePrincipale {
-    private JPanel Main;
-    private JTabbedPane tabbedPaneJoueur;
-    private JTabbedPane tabbedPaneTournoi;
-    private JTabbedPane tabbedPaneMatch;
-    private JTabbedPane tabbedPaneArbitre;
-    private JLabel informationJoueurLabel;
-    private JLabel nomJoueurLabel;
-    private JLabel emailLabel;
-    private JLabel numéroDeLicenceLabel;
-    private JTextField textFieldNomJoueur;
-    private JTextField textFieldEmail;
-    private JTextField textFieldNumLicence;
-    private JButton enregistrerButton;
-    private JTable table1;
-    private JButton mettreÀJourButton;
-    private JButton supprimerButton;
-    private JButton rechercheButton;
-    private JTextField textFieldSearch;
-    private JLabel telephoneLabel;
-    private JTextField textFieldTelephone;
-    private JTextArea textAreaDescription;
-    private JLabel descriptionLabel;
-    private JPanel MainJoueur;
-    Connection con;
-    PreparedStatement pst;
+    public JPanel Main;
+    public JTabbedPane tabbedPaneJoueur;
+    public JTabbedPane tabbedPaneTournoi;
+    public JTabbedPane tabbedPaneMatch;
+    public JTabbedPane tabbedPaneArbitre;
+    public JLabel informationJoueurLabel;
+    public JLabel nomJoueurLabel;
+    public JLabel emailLabel;
+    public JLabel numéroDeLicenceLabel;
+    public JTextField textFieldNomJoueur;
+    public JTextField textFieldEmail;
+    public JTextField textFieldNumLicence;
+    public JButton enregistrerButton;
+    public JTable table1;
+    public JButton mettreÀJourButton;
+    public JButton supprimerButton;
+    public JButton rechercheButton;
+    public JTextField textFieldSearch;
+    public JLabel telephoneLabel;
+    public JTextField textFieldTelephone;
+    public JTextArea textAreaDescription;
+    public JLabel descriptionLabel;
+    public JPanel MainJoueur;
+    public CommonSwing commonSwing = new CommonSwing();
+    public PreparedStatement pst;
 
 
     public static void main(String[] args) {
+
         JFrame frame = new JFrame("Application Tennis");
         frame.setContentPane(new FenetrePrincipale().Main);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -44,176 +45,31 @@ public class FenetrePrincipale {
     }
 
     public FenetrePrincipale() {
-        connect();
+        commonSwing.connect();
         enregistrerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                String joueur_name;
-                String joueur_email;
-                String joueur_phone ;
-                String joueur_numLicence;
-                String joueur_description;
-
-                joueur_name=textFieldNomJoueur.getText();
-                joueur_email=textFieldEmail.getText();
-                joueur_phone=textFieldTelephone.getText();
-                joueur_numLicence=textFieldNumLicence.getText();
-                joueur_description=textAreaDescription.getText();
-
-
-
-                try {
-                   /* INSERT INTO `onlinebanking`.`joueur`
-                    (`joueur_id`,
-`num_licence`,
-`description`,
-`email`,
-`name`,
-`phone`,
-`id`,
-`user_id`)
-                    VALUES
-                            (<{joueur_id: }>,
-<{num_licence: }>,
-<{description: }>,
-<{email: }>,
-<{name: }>,
-<{phone: }>,
-<{id: }>,
-<{user_id: }>);*/
-                    pst = con.prepareStatement("insert into onlinebanking.joueur(num_licence,description,email,name,phone)values(?,?,?,?,?)");
-                    pst.setString(1, joueur_numLicence);
-                    pst.setString(2, joueur_description);
-                    pst.setString(3, joueur_email);
-                    pst.setString(4, joueur_name);
-                    pst.setString(5, joueur_phone);
-                    pst.executeUpdate();
-                    JOptionPane.showMessageDialog(null, "Record Addedddd!!!!!");
-                    table_load();
-                    textFieldNumLicence.setText("");
-                    textAreaDescription.setText("");
-                    textFieldEmail.setText("");
-                    textFieldNomJoueur.setText("");
-                    textFieldTelephone.setText("");
-                    textFieldNomJoueur.requestFocus();
-                }
-
-                catch (SQLException e1)
-                {
-
-                    e1.printStackTrace();
-                }
-
-
-
+                enregistrerJoueurJDBC();
             }
         });
+
         rechercheButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-/**
- SELECT `joueur`.`joueur_id`,
- `joueur`.`num_licence`,
- `joueur`.`description`,
- `joueur`.`email`,
- `joueur`.`name`,
- `joueur`.`phone`,
- `joueur`.`id`,
- `joueur`.`user_id`
- FROM `onlinebanking`.`joueur`;
- SELECT * FROM onlinebanking.joueur;
- */
 
-                try {
-
-                    String joueurId = textFieldSearch.getText();
-
-                    pst = con.prepareStatement("select num_licence,description,email,name,phone from onlinebanking.joueur where id = ?");
-                    pst.setString(1, joueurId);
-                    ResultSet rs = pst.executeQuery();
-
-                    if(rs.next()==true)
-                    {
-                        String num_licence = rs.getString(1);
-                        String description = rs.getString(2);
-                        String email = rs.getString(3);
-                        String name = rs.getString(4);
-                        String phone = rs.getString(5);
-
-                        textFieldNumLicence.setText(num_licence);
-                        textAreaDescription.setText(description);
-                        textFieldEmail.setText(email);
-                        textFieldNomJoueur.setText(name);
-                        textFieldTelephone.setText(phone);
-
-                    }
-                    else
-                    {
-                        textFieldNumLicence.setText("");
-                        textAreaDescription.setText("");
-                        textFieldEmail.setText("");
-                        textFieldNomJoueur.setText("");
-                        textFieldTelephone.setText("");
-                        JOptionPane.showMessageDialog(null,"Invalid joueur No");
-
-                    }
-                }
-                catch (SQLException ex)
-                {
-                    ex.printStackTrace();
-                }
-
+                rechercherJoueurJDBC();
 
 
             }
         });
+
         mettreÀJourButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
 
-                String joueur_name;
-                String joueur_email;
-                String joueur_phone ;
-                String joueur_numLicence;
-                String joueur_description;
-                String joueur_id;
-
-                joueur_name=textFieldNomJoueur.getText();
-                joueur_email=textFieldEmail.getText();
-                joueur_phone=textFieldTelephone.getText();
-                joueur_numLicence=textFieldNumLicence.getText();
-                joueur_description=textAreaDescription.getText();
-                joueur_id=textFieldSearch.getText();
-
-
-
-
-                try {
-                    pst = con.prepareStatement("update onlinebanking.joueur set num_licence = ?,description = ?,email = ?,name = ?,phone = ? where id = ?");
-                    //num_licence,description,email,name,phone)
-                    pst.setString(1, joueur_numLicence);
-                    pst.setString(2, joueur_description);
-                    pst.setString(3, joueur_email);
-                    pst.setString(4, joueur_name);
-                    pst.setString(5, joueur_phone);
-                    pst.setString(6, joueur_id);
-                    pst.executeUpdate();
-                    JOptionPane.showMessageDialog(null, "Record updated!!!!!");
-                    table_load();
-                    textFieldNumLicence.setText("");
-                    textAreaDescription.setText("");
-                    textFieldEmail.setText("");
-                    textFieldNomJoueur.setText("");
-                    textFieldTelephone.setText("");
-                    textFieldNomJoueur.requestFocus();
-                }
-
-                catch (SQLException e1)
-                {
-                    e1.printStackTrace();
-                }
+                mettreAJourJoueurJDBC();
             }
 
 
@@ -224,63 +80,163 @@ public class FenetrePrincipale {
             public void actionPerformed(ActionEvent e) {
 
 
-                String joueurId = textFieldSearch.getText();
-
-                try {
-                    pst = con.prepareStatement("delete from onlinebanking.joueur  where id = ?");
-
-                    pst.setString(1, joueurId);
-
-                    pst.executeUpdate();
-                    JOptionPane.showMessageDialog(null, "Record Deleteeeeee!!!!!");
-                    table_load();
-
-                    textFieldNumLicence.setText("");
-                    textAreaDescription.setText("");
-                    textFieldEmail.setText("");
-                    textFieldNomJoueur.setText("");
-                    textFieldTelephone.setText("");
-                    textFieldNomJoueur.requestFocus();
-
-
-                }
-
-                catch (SQLException e1)
-                {
-
-                    e1.printStackTrace();
-                }
-
+                supprimerJoueurJDBC();
 
 
             }
         });
     }
 
-    void table_load()
-    {
-        try
-        {
-            pst = con.prepareStatement("select * from onlinebanking.joueur");
-            ResultSet rs = pst.executeQuery();
-            table1.setModel(DbUtils.resultSetToTableModel(rs));
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
-    }
-    public void connect (){
+    public void supprimerJoueurJDBC() {
+        String joueurId = textFieldSearch.getText();
+
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/onlinebanking","root","");
-            System.out.println("Success");
-        }catch (ClassNotFoundException ex){
-            System.out.println("ClassNotFoundException ici"+ex);
-            ex.printStackTrace();
-        }catch (SQLException ex){
-            System.out.println("SQLException ici"+ex);
+            pst = commonSwing.getCon().prepareStatement("delete from onlinebanking.joueur  where id = ?");
+
+            pst.setString(1, joueurId);
+
+            pst.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Record Deleteeeeee!!!!!");
+            commonSwing.table_load(table1);
+
+            textFieldNumLicence.setText("");
+            textAreaDescription.setText("");
+            textFieldEmail.setText("");
+            textFieldNomJoueur.setText("");
+            textFieldTelephone.setText("");
+            textFieldNomJoueur.requestFocus();
+
+
+        }
+
+        catch (SQLException e1)
+        {
+
+            e1.printStackTrace();
+        }
+    }
+
+    public void mettreAJourJoueurJDBC() {
+        String joueur_name;
+        String joueur_email;
+        String joueur_phone ;
+        String joueur_numLicence;
+        String joueur_description;
+        String joueur_id;
+
+        joueur_name=textFieldNomJoueur.getText();
+        joueur_email=textFieldEmail.getText();
+        joueur_phone=textFieldTelephone.getText();
+        joueur_numLicence=textFieldNumLicence.getText();
+        joueur_description=textAreaDescription.getText();
+        joueur_id=textFieldSearch.getText();
+
+
+        try {
+            pst = commonSwing.getCon().prepareStatement("update onlinebanking.joueur set num_licence = ?,description = ?,email = ?,name = ?,phone = ? where id = ?");
+            //num_licence,description,email,name,phone)
+            pst.setString(1, joueur_numLicence);
+            pst.setString(2, joueur_description);
+            pst.setString(3, joueur_email);
+            pst.setString(4, joueur_name);
+            pst.setString(5, joueur_phone);
+            pst.setString(6, joueur_id);
+            pst.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Record updated!!!!!");
+            commonSwing.table_load(table1);
+            textFieldNumLicence.setText("");
+            textAreaDescription.setText("");
+            textFieldEmail.setText("");
+            textFieldNomJoueur.setText("");
+            textFieldTelephone.setText("");
+            textFieldNomJoueur.requestFocus();
+        }
+
+        catch (SQLException e1)
+        {
+            e1.printStackTrace();
+        }
+    }
+
+    public void rechercherJoueurJDBC() {
+        try {
+
+            String joueurId = textFieldSearch.getText();
+
+            pst = commonSwing.getCon().prepareStatement("select num_licence,description,email,name,phone from onlinebanking.joueur where id = ?");
+            pst.setString(1, joueurId);
+            ResultSet rs = pst.executeQuery();
+
+            if(rs.next()==true)
+            {
+                String num_licence = rs.getString(1);
+                String description = rs.getString(2);
+                String email = rs.getString(3);
+                String name = rs.getString(4);
+                String phone = rs.getString(5);
+
+                textFieldNumLicence.setText(num_licence);
+                textAreaDescription.setText(description);
+                textFieldEmail.setText(email);
+                textFieldNomJoueur.setText(name);
+                textFieldTelephone.setText(phone);
+
+            }
+            else
+            {
+                textFieldNumLicence.setText("");
+                textAreaDescription.setText("");
+                textFieldEmail.setText("");
+                textFieldNomJoueur.setText("");
+                textFieldTelephone.setText("");
+                JOptionPane.showMessageDialog(null,"Invalid joueur No");
+
+            }
+        }
+        catch (SQLException ex)
+        {
             ex.printStackTrace();
         }
     }
+
+    public void enregistrerJoueurJDBC() {
+        String joueur_name;
+        String joueur_email;
+        String joueur_phone ;
+        String joueur_numLicence;
+        String joueur_description;
+
+        joueur_name=textFieldNomJoueur.getText();
+        joueur_email=textFieldEmail.getText();
+        joueur_phone=textFieldTelephone.getText();
+        joueur_numLicence=textFieldNumLicence.getText();
+        joueur_description=textAreaDescription.getText();
+
+
+        try {
+            pst = commonSwing.getCon().prepareStatement("insert into onlinebanking.joueur(num_licence,description,email,name,phone)values(?,?,?,?,?)");
+            pst.setString(1, joueur_numLicence);
+            pst.setString(2, joueur_description);
+            pst.setString(3, joueur_email);
+            pst.setString(4, joueur_name);
+            pst.setString(5, joueur_phone);
+            pst.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Record Addedddd!!!!!");
+            commonSwing.table_load(table1);
+            textFieldNumLicence.setText("");
+            textAreaDescription.setText("");
+            textFieldEmail.setText("");
+            textFieldNomJoueur.setText("");
+            textFieldTelephone.setText("");
+            textFieldNomJoueur.requestFocus();
+        }
+
+        catch (SQLException e1)
+        {
+
+            e1.printStackTrace();
+        }
+    }
+
+
 }
---
+++ UserFront/src/main/java/com/userfront/jswing/common/CommonSwing.java
@@ -0,0 +1,58 @@
+package com.userfront.jswing.common;
+
+import net.proteanit.sql.DbUtils;
+
+import javax.swing.*;
+import java.sql.*;
+
+public class CommonSwing {
+    Connection con;
+    PreparedStatement pst;
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+
+    public PreparedStatement getPst() {
+        return pst;
+    }
+
+    public void setPst(PreparedStatement pst) {
+        this.pst = pst;
+    }
+
+    public CommonSwing() {
+    }
+
+    public void table_load(JTable table1)
+    {
+        try
+        {
+            pst = this.getCon().prepareStatement("select * from onlinebanking.joueur");
+            ResultSet rs = pst.executeQuery();
+            table1.setModel(DbUtils.resultSetToTableModel(rs));
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void connect (){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            setCon(DriverManager.getConnection("jdbc:mysql://localhost:3306/onlinebanking","root",""));
+            System.out.println("Success");
+        }catch (ClassNotFoundException ex){
+            System.out.println("ClassNotFoundException ici"+ex);
+            ex.printStackTrace();
+        }catch (SQLException ex){
+            System.out.println("SQLException ici"+ex);
+            ex.printStackTrace();
+        }
+    }
+}
